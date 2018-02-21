91e1ca8d256c83ab165b81cc117ac10a38626ea9
Katelyn Stamas <kate@Katelyns-MBP.fios-router.home>
2018-02-20 19:42:04 -0500

    Refactor input fields to separate component

--- client/src/Index.jsx
+++ client/src/Index.jsx
@@ -1,45 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Input from './components/Input.jsx';
 import axios from 'axios';
 
 class Index extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      address: '',
-      transactions: [],
-    }
-  }
-
-  updateAddress(e) {
-    this.setState({ address: e.target.value })
-  }
-
-  getLastFiveTransInfo(e) {
-    e.preventDefault();
-
-    axios({
-      method: 'post',
-      url: '/transactions',
-      data: { address: this.state.address }
-    })
-      .then((response) => {
-        this.setState({ transactions: response.data.result});
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-
-  }
-
   render() {
     return (
       <div>
-
-        <form onSubmit={this.getLastFiveTransInfo.bind(this)}>
-          Enter address: <input type="text" name="address" onChange={this.updateAddress.bind(this)} /><br />
-          <input type="submit" value="Search" />
-        </form>
+        <Input />
       </div>
     )
   }
