825c7b7a8ed0871c095935f35dce325fe1098450
Katelyn Stamas <kate@Katelyns-MBP.fios-router.home>
2018-02-20 19:19:16 -0500

    Change API being used

--- client/src/Index.jsx
+++ client/src/Index.jsx
@@ -3,44 +3,27 @@ import ReactDOM from 'react-dom';
 import axios from 'axios';
 
 class Index extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      hashes: []
-    }
-  }
 
   getLastFiveTransInfo() {
     this.getTransactions();
   }
 
   getTransactions() {
-    const url = `https://api.blockcypher.com/v1/eth/main/addrs/738d145faabb1e00cf5a017588a9c0f998318012`;
+    const address = '0xddbd2b932c763ba5b1b7ae3b362eac3e8d40121a';
     const hashes = [];
 
-    axios.get(url)
-      .then((response) => {
-        for (let i = 0; i < 5; i++) {
-          setTimeout(
-            this.getTransactionInfo(response.data.txrefs[i].tx_hash),
-            350
-          );
-        }
-      })
-      .catch((error) => {
-        console.log(`Error: ${error}`);
-      });
-  }
-
-  getTransactionInfo(hash) {
-
-    axios.get(`https://api.blockcypher.com/v1/eth/main/txs/${hash}`)
+    axios({
+      method: 'post',
+      url: '/transactions',
+      data: { address: address }
+    })
       .then((response) => {
         console.log(response.data);
       })
-      .catch((error) => {
-        console.log(`Error: ${error}`);
+      .catch((err) => {
+        console.log(err);
       });
+
   }
 
   render() {
--- server/index.js
+++ server/index.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+require('dotenv').config();
 
 const app = express();
 const port = 1337;
 
 app.use(express.static(path.join(__dirname, '/../client/dist')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/transactions', (req, res) => {
+  const url = `http://api.etherscan.io/api?module=account&action=txlist&address=${req.body.address}&sort=asc&&apikey=${process.env.API_KEY}`;
+
+  axios.get(url)
+    .then((response) => {
+      console.log(response.data)
+    })
+    .catch((error) => {
+      console.log(`Error: ${error}`);
+    });
+
+});
 
 app.listen(port, () => console.log(`Listening on ${port}`));
