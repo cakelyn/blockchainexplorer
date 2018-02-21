42d874f6815c447e8f4d4ef096dc8dade3d2f271
Katelyn Stamas <kate@Katelyns-MBP.fios-router.home>
2018-02-20 19:59:33 -0500

    Make Input functional

--- client/src/Index.jsx
+++ client/src/Index.jsx
@@ -1,13 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Input from './components/Input.jsx';
+import Transactions from './components/Transactions.jsx';
 import axios from 'axios';
 
 class Index extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      address: '',
+      transactions: [],
+    }
+    this.updateAddress = this.updateAddress.bind(this);
+    this.getLastFiveTransInfo = this.getLastFiveTransInfo.bind(this);
+  }
+
+  updateAddress(e) {
+    this.setState({ address: e.target.value })
+  }
+
+  getLastFiveTransInfo(e) {
+    e.preventDefault();
+
+    axios({
+      method: 'post',
+      url: '/transactions',
+      data: { address: this.state.address }
+    })
+      .then((response) => {
+        this.setState({ transactions: response.data.result});
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+  }
+
   render() {
     return (
       <div>
-        <Input />
+        <Input updateAddress={this.updateAddress} getLastFiveTransInfo={this.getLastFiveTransInfo} />
+        <Transactions />
       </div>
     )
   }
--- client/src/components/Input.jsx
+++ client/src/components/Input.jsx
@@ -1,46 +1,15 @@
 import React from 'react';
-import axios from 'axios';
 
-class Input extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      address: '',
-      transactions: [],
-    }
-  }
+const Input = (props) => {
 
-  updateAddress(e) {
-    this.setState({ address: e.target.value })
-  }
-
-  getLastFiveTransInfo(e) {
-    e.preventDefault();
-
-    axios({
-      method: 'post',
-      url: '/transactions',
-      data: { address: this.state.address }
-    })
-      .then((response) => {
-        this.setState({ transactions: response.data.result});
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-
-  }
-
-  render() {
     return (
       <div>
-        <form onSubmit={this.getLastFiveTransInfo.bind(this)}>
-          Enter address: <input type="text" name="address" onChange={this.updateAddress.bind(this)} /><br />
+        <form onSubmit={props.getLastFiveTransInfo.bind(this)}>
+          Enter address: <input type="text" name="address" onChange={props.updateAddress.bind(this)} /><br />
           <input type="submit" value="Search" />
         </form>
       </div>
     )
-  }
 
 }
 
