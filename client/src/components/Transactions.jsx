d90f3959060cccb83f46f572f0b0c8c2613ce040
Katelyn Stamas <kate@Katelyns-MBP.fios-router.home>
2018-02-20 21:58:30 -0500

    Remove console logs

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -5,7 +5,7 @@ const Transactions = (props) => {
 
   // map will only run if there are transactions stored in the state
   // and only the last 5 transactions will be mapped
-  console.log(props.transactions)
+
   return (
     <div>
       { props.transactions.length > 0 ?
